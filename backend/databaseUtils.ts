8d6044699032f01e033d67c43539ff52ec5085fc
Ricardo Hendrichs <71273811+Dohruba@users.noreply.github.com>
2024-06-18 21:03:31 +0200

    Fill and update of programming languages and developers table

--- backend/databaseUtils.ts
+++ backend/databaseUtils.ts
@@ -20,8 +20,8 @@ const octokit = new Octokit({
 
 const fillDatabase = async () => {
     fillDevelopersTable()
-      // .then(() => console.log("Filling Repositories"))
-      // .then(() => fillRepositoriesTable())
+      .then(() => console.log("Filling Repositories"))
+      .then(() => fillRepositoriesTable())
       .then(() => console.log("Filling Programming Languages"))
       .then(() => fillLanguagesTable())
       .then(() => console.log("Database started"));
@@ -116,7 +116,7 @@ const fillDatabase = async () => {
     const languagesInDB = await pool.query("SELECT * FROM programminglanguages")
     .then((res) => res.rows.map((row) => row.language));
     //Array developers per language
-    const devLanguages: { [key: string]: string[] } = {};
+    const devLanguagesAPI: [string, string[]][] = [];
     //Set of languages from the API
     let languagesFromAPI = new Set<string>();
 
@@ -127,23 +127,23 @@ const fillDatabase = async () => {
       const devRepos = repositoriesOnDB.rows.filter((row) => {
         return row.developer === dev
       });
-      const tmp:string[] = ["TypeScript", "JavaScript", "HTML", "C", "Vue", "Java", "CSS", "Objective-C", "Python", "Ruby", "chacha"];
-      //Get languages for each repo
       for (const repo of devRepos) {
-        if(dev !== "rueckemann" && dev !== "goekhanm" ) continue;
-        const repoLangs:string[] = [tmp[Math.floor(Math.random() * tmp.length)]];
-        //await octokit.request(repo.languages_link)
-          // .then((res: any) => Object.keys(res.data));
+        const repoLangs:string[] = 
+        await octokit.request(repo.languages_link)
+          .then((res: any) => Object.keys(res.data));
         for (const lang of repoLangs) {
           languagesFromAPI.add(lang);
-          devLanguages[lang] = devLanguages[lang] || [];
-          if(!devLanguages[lang].includes(dev))
-            devLanguages[lang].push(dev);
+          let langEntry = devLanguagesAPI.find((entry) => entry[0] === lang);
+          if (!langEntry) {
+            langEntry = [lang, []];
+            devLanguagesAPI.push(langEntry);
+          }
+          if (!langEntry[1].includes(dev)) {
+            langEntry[1].push(dev);
+          }
         }
       }
     }
-
-    console.log("devLanguages", devLanguages);
 
     const languagesToAdd:string[] = Array.from(languagesFromAPI)
       .filter((lang) => !languagesInDB.includes(lang));
@@ -157,38 +157,44 @@ const fillDatabase = async () => {
       await pool.query("DELETE FROM programminglanguages WHERE language = $1", [lang]);
       console.log(`Deleted ${lang}`);
     }
-    const queryLanguages = await pool.query("SELECT * FROM programminglanguages")
+    const queryLanguages: [string, string[]][] = await pool.query("SELECT * FROM programminglanguages")
     .then((res) => res.rows.map((row) => {
       let devs:string[] = row.developers;
       let lang:string = row.language;
       return [lang, devs];
     }));
 
-    console.log(queryLanguages)
+    console.log("query",queryLanguages)
+    console.log("Languagesdevs", devLanguagesAPI)
     for(const [lang, devs] of queryLanguages){
-      console.log("Languages: ", lang);
-      console.log("Devs in DB for language", devs);
-    }
-    
-      //I have to update the developers for each language
-      //I have the updated languages (and their devs) from the database and the languages from the api (and their devs)
-  // for (let i = 0; i < updatedLanguages.length; i++){
-      // const lang:string = Object.keys(updatedLanguages[i])[0];
-      // const devs = updatedLanguages[i][];
-      // const updatedDevs:string[] = devLanguages[lang];
-      // console.log("Languages: ", lang);
-      // console.log("Devs in DB for language", updatedLanguages.map((obj) => obj[lang]));
-      // console.log("Devs updated", updatedDevs);
-      // const devsToAdd = updatedDevs.filter((dev) => !devs.includes(dev));
-      // const devsToRemove = devs.filter((dev) => !updatedDevs.includes(dev));
-      // for (const dev of devsToAdd){
-      //   await pool.query("UPDATE programminglanguages SET developers = array_append(developers, $2) WHERE language = $1", [lang, dev]);
-      //   console.log(`Added ${dev} to ${lang}`);
-      // }
-      // for (const dev of devsToRemove){
-      //   await pool.query("UPDATE programminglanguages SET developers = array_remove(developers, $2) WHERE language = $1", [lang, dev]);
-      //   console.log(`Removed ${dev} from ${lang}`);
-      // }
-    // }
+      const updatedDevsAPI = devLanguagesAPI.find((entry) => entry[0] === lang);
+      console.log("UpdatedDevsAPI", updatedDevsAPI);
+      let devsToAdd:string[] = [];
+      let devsToRemove:string[] = [];
+
+
+      if(updatedDevsAPI){
+        if(!devs){
+          devsToAdd = updatedDevsAPI[1];
+          devsToRemove = [];
+          console.log("No devs in DB");
+        }else{
+          devsToAdd = updatedDevsAPI[1].filter((dev) => !devs.includes(dev));
+          devsToRemove = devs.filter((dev) => !updatedDevsAPI[1].includes(dev));
+          console.log("Devs in DB", devs);
+        }
+      }
+      
+      for (const dev of devsToAdd) {
+        await pool.query("UPDATE programminglanguages SET developers = array_append(developers, $2) WHERE language = $1", [lang, dev]);
+        console.log(`Added ${dev} to ${lang}`);
+      }
+      for (const dev of devsToRemove) {
+        await pool.query("UPDATE programminglanguages SET developers = array_remove(developers, $2) WHERE language = $1", [lang, dev]);
+        console.log(`Removed ${dev} from ${lang}`);
+      }
+    } 
+
   };
+  
   export { fillDatabase};
