95a64017cf454952c63e217b8cef20f0d35b9065
Ricardo Hendrichs <71273811+Dohruba@users.noreply.github.com>
2024-06-19 09:57:53 +0200

    Put Database API in separate script
    Updated API to respond to bad requests or not found.

--- backend/index.ts
+++ backend/index.ts
@@ -1,60 +1,18 @@
 import express from "express";
-import { Pool} from "pg";
-import { fillDatabase } from "./databaseUtils"
+import { fillDatabase } from "./utils/databaseUtils"
 import dotenv from "dotenv";
+import { MembersDatabaseAPI } from "./utils/membersApi";
 
 const args = process.argv.slice(2);
-
 dotenv.config();
-
 const app = express();
-const port = 8000;
-
-const { Octokit } = require("@octokit/rest");
-const octokit = new Octokit({
-  auth: process.env.TOKEN_SECRET,
-  baseUrl: "https://api.github.com",
-});
-const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: 5432,
-});
-
-pool.on("connect", () => {
-  console.log("Connected to the database");
-});
+const backendPort = 8000;
 
 app.use(express.json());
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const membersDatabaseAPI = new MembersDatabaseAPI(app);
+app.listen(backendPort, () => {
+  console.log(`Server is running on http://localhost:${backendPort}`);
 if(args[0] !== 'false') fillDatabase();
 });
 
-app.get("/members", async (req, res) => {
-  const { rows } = await pool.query("SELECT * FROM developers");
-  res.json(rows);
-});
-
-app.get("/members/:id", async (req, res) => {
-  const { id } = req.params;
-  const queryText = "SELECT * FROM developers WHERE id = $1";
-  const { rows } = await pool.query(queryText, [id]);
-  res.json(rows);
-});
-
-app.get("/languages", async (req, res) => {
-  const queryText = "SELECT language FROM programmingLanguages";
-  const { rows } = await pool.query(queryText);
-  res.json(rows);
-});
-
-app.get("/members/p_language/:name", async (req, res) => {
-  let { name } = req.params;
-  const queryText = "SELECT developers FROM programmingLanguages WHERE language = $1";
-  let { rows } = await pool.query(queryText, [name]);
-  res.json(rows);
-});
+membersDatabaseAPI.setApp(app);
