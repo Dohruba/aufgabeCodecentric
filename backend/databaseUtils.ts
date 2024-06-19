8e82e132c7df10942c6f6ebe5bb8f1cfaa62c9b5
Ricardo Hendrichs <71273811+Dohruba@users.noreply.github.com>
2024-06-19 09:23:50 +0200

    Improved readability
    Changed variable names and cleaned up code

--- backend/databaseUtils.ts
+++ backend/databaseUtils.ts
@@ -28,161 +28,147 @@ const fillDatabase = async () => {
   };
   
   const fillDevelopersTable = async () => {
-    const data = JSON.parse(fs.readFileSync("members.json", "utf-8"));
+    const devsDataAPI = JSON.parse(fs.readFileSync("members.json", "utf-8"));
+    const devNamesInAPI: string[] = devsDataAPI.map((obj: any) => obj.login);
   
-    const currentDevs = await pool.query("SELECT * FROM developers");
-    const namesInDB: string[] = currentDevs.rows.map((row) => row["name"]);
-    const namesFromAPI: string[] = data.map((obj: any) => obj.login);
-    const devsToAdd = namesFromAPI.filter((name) => !namesInDB.includes(name));
-    const devsToRemove = namesInDB.filter((name) => !namesFromAPI.includes(name));
+    const devsDataDB = await pool.query("SELECT * FROM developers");
+    const devNamesInDB: string[] = devsDataDB.rows.map((row) => row["name"]);
+
+    const devsToAdd = devNamesInAPI.filter((name) => !devNamesInDB.includes(name));
+    const devsToRemove = devNamesInDB.filter((name) => !devNamesInAPI.includes(name));
   
     for (const name of devsToRemove) {
       await pool.query("DELETE FROM developers WHERE name = $1", [name]);
-      console.log(`Deleted ${name}`);
       await pool.query("DELETE FROM repositories WHERE developer = $1", [name]);
-      console.log(`Deleted ${name}'s repositories`);
       await pool.query(
         "UPDATE programminglanguages SET developers = array_remove(developers, $1)",
-        [name]
-      );
-      console.log(`Removed ${name} from languages`);
+        [name]);
     }
     for (const name of devsToAdd) {
-      const dev = data.find((obj: any) => obj.login === name);
+      const developer = devsDataAPI.find((obj: any) => obj.login === name);
       await pool.query(
         "INSERT INTO developers (id, name, reposlink, githublink) VALUES ($1, $2, $3, $4)",
-        [dev.id, dev.login, dev.repos_url, dev.html_url]
+        [developer.id, developer.login, developer.repos_url, developer.html_url]
       );
     }
   };
   
   const fillRepositoriesTable = async () => {
     const developers = await pool.query("SELECT * FROM developers");
-    const repositories = await pool.query("SELECT * FROM repositories");
-    const owners: string[] = developers.rows.map((row) => row.name);
-    const reposNamesDB: string[] = repositories.rows.map((row) => row.name);
+    const reposInDB = await pool.query("SELECT * FROM repositories");
+    const devNames: string[] = developers.rows.map((row) => row.name);
   
-    for (const owner of owners) {
-      const ownerReposDB: string[] = repositories.rows
-        .filter((row) => row.developer === owner)
+    for (const devName of devNames) {
+      const ownerNamesDB: string[] = reposInDB.rows
+        .filter((row) => row.developer === devName)
         .map((row) => row.name);
-  
-      const reposFromAPI =  await octokit.repos.listForUser({
-        username: owner,
+
+      const reposFromAPI = await octokit.repos.listForUser({
+        username: devName,
       });
-  
+
       const ownerReposAPI: string[] = reposFromAPI.data.map(
         (repo: any) => repo.name
       );
       const reposToAdd = ownerReposAPI.filter(
-        (name) => !ownerReposDB.includes(name)
+        (name) => !ownerNamesDB.includes(name)
       );
-      const reposToRemove = ownerReposDB.filter(
+      const reposToRemove = ownerNamesDB.filter(
         (name) => !ownerReposAPI.includes(name)
       );
-  if(reposToRemove.length === 0)
-        // await pool.query("DELETE FROM repositories WHERE name = ANY($1)", [reposToRemove]);
-      for (const repo of reposToRemove) {
-        await pool.query("DELETE FROM repositories WHERE name = $1", [repo]);
-        console.log(`Deleted ${repo}`);
-      }
-  if(reposToAdd.length === 0)
-      for (const repo of reposToAdd) {
-        const repoData = reposFromAPI.data.find((obj: any) => obj.name === repo);
-        if(repoData)
-        await pool.query(
-          "INSERT INTO repositories (id, name, developer, languages_link, url) VALUES ($1, $2, $3, $4, $5)",
-          [
-            repoData.id,
-            repoData.name,
-            owner,
-            repoData.languages_url,
-            repoData.html_url,
-          ]
-        );
-        console.log(`Added ${repo}`);
-      }
+
+      if (reposToRemove.length !== 0)
+        for (const repo of reposToRemove) {
+          await pool.query("DELETE FROM repositories WHERE name = $1", [repo]);
+        }
+      if (reposToAdd.length !== 0)
+        for (const repo of reposToAdd) {
+          const repoData = reposFromAPI.data.find(
+            (obj: any) => obj.name === repo
+          );
+          if (repoData)
+            await pool.query(
+              "INSERT INTO repositories (id, name, developer, languages_link, url) VALUES ($1, $2, $3, $4, $5)",
+              [
+                repoData.id,
+                repoData.name,
+                devName,
+                repoData.languages_url,
+                repoData.html_url,
+              ]
+            );
+        }
     }
   };
   
   const fillLanguagesTable = async () => {
-    //Array of developer names
-    const developerNamesOnDB: string[] = await pool
+    const devNamesInDB: string[] = await pool
       .query("SELECT * FROM developers")
       .then((res) => res.rows.map((row) => row.name));
-    //Select all repositories
-    const repositoriesOnDB = await pool.query("SELECT * FROM repositories");
-    //Array of languages in the database
+    const reposInDB = await pool.query("SELECT * FROM repositories");
     const languagesInDB = await pool.query("SELECT * FROM programminglanguages")
     .then((res) => res.rows.map((row) => row.language));
-    //Array developers per language
-    const devLanguagesAPI: [string, string[]][] = [];
-    //Set of languages from the API
-    let languagesFromAPI = new Set<string>();
+    //Array of [language, [developers]]
+    const languageDevsAPI: [string, string[]][] = [];
+    let allLanguagesInAPI = new Set<string>();
 
-    //Get languages for each repo and for each dev
-    for (const dev of developerNamesOnDB) {
-      //Get all repos for the developer
-      const devRepos = repositoriesOnDB.rows.filter((row) => {
-        return row.developer === dev
+    for (const devName of devNamesInDB) {
+      const devRepos = reposInDB.rows.filter((row) => {
+        return row.developer === devName
       });
       for (const repo of devRepos) {
+        //Languages in repository
         const repoLangs:string[] = 
         await octokit.request(repo.languages_link)
           .then((res: any) => Object.keys(res.data));
-
-        for (let lang of repoLangs) {
+          for (let lang of repoLangs) {
+          //Replace # with sharp, to facilitate DB queries
           if(lang.includes("#")) {
             lang = lang.replace("#", "sharp");
           }
-          languagesFromAPI.add(lang);
-          let langEntry = devLanguagesAPI.find((entry) => entry[0] === lang);
+          allLanguagesInAPI.add(lang);
+          let langEntry = languageDevsAPI.find((entry) => entry[0] === lang);
           if (!langEntry) {
             langEntry = [lang, []];
-            devLanguagesAPI.push(langEntry);
+            languageDevsAPI.push(langEntry);
           }
-          if (!langEntry[1].includes(dev)) {
-            langEntry[1].push(dev);
+          if (!langEntry[1].includes(devName)) {
+            langEntry[1].push(devName);
           }
         }
       }
     }
 
-    const languagesToAdd:string[] = Array.from(languagesFromAPI)
+    const languagesToAdd:string[] = Array.from(allLanguagesInAPI)
       .filter((lang) => !languagesInDB.includes(lang));
     const languagesToRemove:string[] = languagesInDB
-      .filter((lang) => !Array.from(languagesFromAPI).includes(lang));
+      .filter((lang) => !Array.from(allLanguagesInAPI).includes(lang));
+
     for (const lang of languagesToAdd){
       await pool.query("INSERT INTO programminglanguages (language) VALUES ($1)", [lang]);
-      console.log(`Added ${lang}`);
     }
     for (const lang of languagesToRemove){
       await pool.query("DELETE FROM programminglanguages WHERE language = $1", [lang]);
-      console.log(`Deleted ${lang}`);
     }
-    const queryLanguages: [string, string[]][] = await pool.query("SELECT * FROM programminglanguages")
+    const updatedLangsInDB: [string, string[]][] = await pool.query("SELECT * FROM programminglanguages")
     .then((res) => res.rows.map((row) => {
       let devs:string[] = row.developers;
       let lang:string = row.language;
       return [lang, devs];
     }));
 
-    for(const [lang, devs] of queryLanguages){
-      const updatedDevsAPI = devLanguagesAPI.find((entry) => entry[0] === lang);
-      console.log("UpdatedDevsAPI", updatedDevsAPI);
+    for(const [lang, devsInDB] of updatedLangsInDB){
+      const languageDevs = languageDevsAPI.find((entry) => entry[0] === lang);
       let devsToAdd:string[] = [];
       let devsToRemove:string[] = [];
 
-
-      if(updatedDevsAPI){
-        if(!devs){
-          devsToAdd = updatedDevsAPI[1];
+      if(languageDevs){
+        if(!devsInDB){
+          devsToAdd = languageDevs[1];
           devsToRemove = [];
-          console.log("No devs in DB");
         }else{
-          devsToAdd = updatedDevsAPI[1].filter((dev) => !devs.includes(dev));
-          devsToRemove = devs.filter((dev) => !updatedDevsAPI[1].includes(dev));
-          console.log("Devs in DB", devs);
+          devsToAdd = languageDevs[1].filter((dev) => !devsInDB.includes(dev));
+          devsToRemove = devsInDB.filter((dev) => !languageDevs[1].includes(dev));
         }
       }
       
